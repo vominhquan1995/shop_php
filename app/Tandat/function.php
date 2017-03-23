ff82386409e2ac1cc29013d3666766d58a120803
VoMinhQuan <quan.vo@covisoft.asia>
2017-03-24 02:19:30 +0700

    upload list category admin

--- app/Tandat/function.php
+++ app/Tandat/function.php
@@ -35,17 +35,41 @@ function changeTitle($str){
     $str = str_replace(' ','-',$str); 
     return $str; 
 } 
-function cate_parent($data,$parent=0,$str="--",$select=0){
+function cate_parent($data,$parent=0,$select=0){
   foreach ($data as $val) {
         $id = $val["id"];
         $name = $val["name"];
-        if($val["prarent_id"] == $parent){
-        if($select != 0 && $id == $select){
-            echo "<option value='$id' selected='selected'>$str $name</option>";
-        }else{
-            echo "<option value='$id'>$str $name</option>";
-        }
-             cate_parent($data,$id,$str."--",$select);
+        if($val["prarent_id"] == $parent)
+        {
+            if($select != 0 && $id == $select)
+            {
+                
+            }
+            else
+            {
+                echo "<option value='$id'>$name</option>";
+            }
+            //cate_parent($data,$id,$str."--",$select);
+        }  
+  }
+}
+function list_type($data){
+  foreach ($data as $val) 
+  {
+        $id = $val["id"];
+        $name = $val["name"];
+        $nameparent=null;
+        $prarent_id = $val["prarent_id"];
+        if( $prarent_id != 0)         
+        {    
+            foreach ($data as $getname)
+            {
+              if($prarent_id == $getname["id"])
+              {
+                $nameparent=$getname["name"];
+              }
+            }            
+            echo "<option value='$id'>$nameparent-$name</option>";           
         }  
   }
 }
